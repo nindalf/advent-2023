1359c009b13760d0d6ed4ced51488a6e91b6fc28
Krishna Sundarram <krishnasundarram@monzo.com>
2023-12-03 10:12:59 +0000

    Paste in AOC test macro

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+mod day01;
+
 fn main() {
     println!("Hello, world!");
+}
+
+
+#[macro_export]
+macro_rules! aoctest {
+    ($op_1_test: expr, $op_1: expr, $op_2_test: expr, $op_2: expr) => {
+        #[cfg(test)]
+        mod tests {
+            static TEST_INPUT: &str = include_str!("test-input.txt");
+            static FULL_INPUT: &str = include_str!("input.txt");
+
+            #[test]
+            fn part_1_test() {
+                let output = super::part_1(TEST_INPUT);
+                assert_eq!(output, $op_1_test);
+            }
+
+            #[test]
+            fn part_1() {
+                let output = super::part_1(FULL_INPUT);
+                assert_eq!(output, $op_1);
+            }
+
+            #[test]
+            fn part_2_test() {
+                let output = super::part_2(TEST_INPUT);
+                assert_eq!(output, $op_2_test);
+            }
+
+            #[test]
+            fn part_2() {
+                let output = super::part_2(FULL_INPUT);
+                assert_eq!(output, $op_2);
+            }
+        }
+    };
 }
